75229e2b9eb019dbd81b984cfd1bf42adf92fa17
Ashley Williams <ashley@bocoup.com>
2015-06-04 11:10:04 -0400

    setup component based structure for categories - add require-tree dep - build route for every category, return tree

--- app/index.js
+++ app/index.js
@@ -1,5 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 const app = express();
+
+const require_tree = require('require-tree');
+const modulePath = path.join(__dirname, 'modules');
+const categories = require_tree(modulePath);
 
 app.use(express.static('public'));
 
@@ -8,6 +14,10 @@ app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res){
   res.render('index');
+});
+
+app.get('/:category', function(req, res){
+  res.send(JSON.stringify(categories[req.params.category]));
 });
 
 module.exports = app;
