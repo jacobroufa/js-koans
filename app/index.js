9c6eb1914487f38f1ac660b3b20d2ae3a379ea56
Ashley Williams <ashley@bocoup.com>
2015-06-04 16:44:15 -0400

    wire it up!

--- app/index.js
+++ app/index.js
@@ -14,12 +14,13 @@ app.use(express.static('public'));
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
-app.get('/', function(req, res){
-  res.render('index');
+app.get('/:category', function(req, res){
+  cat_obj = util.build_cat(req.params.category);
+  res.render('category', { category: cat_obj })
 });
 
-app.get('/:category', function(req, res){
-  res.send(util.build_cat(req.params.category));
+app.get('/', function(req, res){
+  res.render('index', { categories: categories });
 });
 
 module.exports = app;
--- app/util/index.js
+++ app/util/index.js
@@ -5,9 +5,10 @@ const md = require('markdown').markdown;
 module.exports = {
   build_cat: function(cat_name) {
     return {
+      name: cat_name,
       instructions: md.toHTML(fs.readFileSync(path.join(__dirname, '../modules/', cat_name, '/README.md'), 'utf-8')),
       questions: fs.readFileSync(path.join(__dirname, '../data/js-assessment/app/', cat_name + '.js'), 'utf-8'),
-      // tests: fs.readFileSync(__dirname + '/../data/js-assessment/tests/app/' + cat_name + '.js', 'utf-8'),
+      tests: fs.readFileSync(__dirname + '/../data/js-assessment/tests/app/' + cat_name + '.js', 'utf-8'),
       solutions: fs.readFileSync(path.join(__dirname, '../data/js-assessment-answers/app/', cat_name + '.js'), 'utf-8')
     }
   }
